3bba096456399ed1280a30d344b5ee64ac71929d
pulcineli <pulcineli@gmail.com>
2017-02-22 14:53:03 -0300

    Delete validation_data.js

--- validation_data.js
+++
@@ -1,3 +0,0 @@
- var data_validation = '{"fb-submit-button":{"hover":{"background-image":""}},"item12_0_radio":{"required":true},"item15_0_radio":{"required":true},"item16_0_radio":{"required":true},"sigpad":{},"item22_0_radio":{"required":true},"item23_0_radio":{"required":true},"item24_0_radio":{"required":true}}';
- var data_jsplugins = '[]';
- var data_cssplugins = '[]';
